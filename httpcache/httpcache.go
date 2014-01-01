aed69b072de90f3dcfb4ef89a3c7be9803b9432e
Rick Olson <technoweenie@gmail.com>
2014-01-01 10:57:37 -0700

    remove the defaultAdapter stuff

--- httpcache/httpcache.go
+++ httpcache/httpcache.go
@@ -16,28 +16,18 @@ var (
 )
 
 type Adapter interface {
+	// Get retrieves a Response for a REST resource by its URL.  The URL should be
+	// the full canonical URL for the resource.  The response will be nil if it is
+	// expired.
 	Get(*http.Request, interface{}) *sawyer.Response
+
+	// Set caches a Response for a resource by its URL.
 	Set(*http.Request, *sawyer.Response, interface{}) error
+
+	// Rels retrieves the hypmermedia for a REST resource by its URL.  The relations
+	// cache doesn't expire with the assumption that web services will provide
+	// redirects as URLs change.
 	Rels(*http.Request) hypermedia.Relations
-}
-
-// Get retrieves a Response for a REST resource by its URL.  The URL should be
-// the full canonical URL for the resource.  The response will be nil if it is
-// expired.
-func Get(req *http.Request, v interface{}) *sawyer.Response {
-	return defaultAdapter().Get(req, v)
-}
-
-// Set caches a Response for a resource by its URL.
-func Set(req *http.Request, res *sawyer.Response, v interface{}) error {
-	return defaultAdapter().Set(req, res, v)
-}
-
-// Rels retrieves the hypmermedia for a REST resource by its URL.  The relations
-// cache doesn't expire with the assumption that web services will provide
-// redirects as URLs change.
-func Rels(req *http.Request) hypermedia.Relations {
-	return defaultAdapter().Rels(req)
 }
 
 func EmptyResponse() *sawyer.Response {
@@ -46,13 +36,6 @@ func EmptyResponse() *sawyer.Response {
 
 func RequestKey(r *http.Request) string {
 	return r.Header.Get(keyHeader) + keySep + r.URL.String()
-}
-
-func defaultAdapter() Adapter {
-	if DefaultAdapter == nil {
-		DefaultAdapter = NewMemoryCache()
-	}
-	return DefaultAdapter
 }
 
 const (
